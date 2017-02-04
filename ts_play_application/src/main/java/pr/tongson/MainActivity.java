58dd9265ddf7882bb037341507ee636ba51a8026
kongming <luzhanneng@linghit.com>
2017-02-04 18:49:47 +0800

    tinker引入

--- ts_play_application/src/main/java/pr/tongson/MainActivity.java
+++ ts_play_application/src/main/java/pr/tongson/MainActivity.java
@@ -2,6 +2,7 @@ package pr.tongson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView show= (TextView) findViewById(R.id.show);
+        int a=5;
+        int b;
+        b=++a;
+        
+        show.setText("b="+b);
+//        show.setText("new:"+"b="+b);
+        
     }
 }
---
+++ ts_play_application/src/main/java/pr/tongson/TongsonApplication.java
@@ -0,0 +1,35 @@
+package pr.tongson;
+
+import android.app.Application;
+
+import com.tencent.tinker.loader.app.ApplicationLike;
+import com.tinkerpatch.sdk.TinkerPatch;
+import com.tinkerpatch.sdk.loader.TinkerPatchApplicationLike;
+
+/**
+ * <b>Project:</b> ${file_name}<br>
+ * <b>Create Date:</b> 2017/2/2<br>
+ * <b>Author:</b> mmc_Kongming_Tongson<br>
+ * <b>Description:</b> <br>
+ */
+public class TongsonApplication extends Application{
+    
+    
+    private ApplicationLike tinkerApplicationLike;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 我们可以从这里获得Tinker加载过程的信息
+        tinkerApplicationLike = TinkerPatchApplicationLike.getTinkerPatchApplicationLike();
+
+        // 初始化TinkerPatch SDK, 更多配置可参照API章节中的,初始化SDK
+        TinkerPatch.init(tinkerApplicationLike)
+                .reflectPatchLibrary()
+                .setPatchRollbackOnScreenOff(true)
+                .setPatchRestartOnSrceenOff(true);
+
+        // 每隔3个小时去访问后台时候有更新,通过handler实现轮训的效果
+        new FetchPatchHandler().fetchPatchWithInterval(3);
+    }
+}
