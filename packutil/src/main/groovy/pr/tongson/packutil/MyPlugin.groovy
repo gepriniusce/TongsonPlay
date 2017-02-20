0869255c1fd758e1d7c67fce8a8d7b744b687b9d
Tongson <289286298@qq.com>
2017-02-20 22:26:05 +0800

    plan B

--- packutil/src/main/groovy/pr/tongson/packutil/MyPlugin.groovy
+++ packutil/src/main/groovy/pr/tongson/packutil/MyPlugin.groovy
@@ -1,7 +1,10 @@
 package pr.tongson.packutil
 
+import com.android.build.gradle.api.ApplicationVariant
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.internal.DefaultDomainObjectSet
+
 /**
  * build完成之后把apk文件拷贝到对应的位置的插件
  */
@@ -11,6 +14,60 @@ public class MyPlugin implements Plugin<Project> {
 
         System.out.println("========================");
         System.out.println("hello 插件!");
+
+        project.afterEvaluate {
+            // 重命名文件名
+            if (project.getPlugins().hasPlugin(AppPlugin)) {
+                DefaultDomainObjectSet<ApplicationVariant> variants = project.android.applicationVariants;
+                BuildFileMoveExtension apkExtension = BuildFileMoveExtension.getConfig(project);
+                String projectName = apkExtension.projectName;
+                boolean isgm = apkExtension.isgm;
+                if (projectName == null || projectName.trim().length() <= 0) {
+                    return;
+                }
+                variants.all { variant ->
+                    variant.outputs.each { output ->
+                        if (output.outputFile != null && output.outputFile.name.endsWith(".apk")
+                                && "patch_signed".equals(variant.buildType.name)) {
+                            def buildTime = new Date().format("yyyy_MM_dd");
+                            def apkname = projectName;
+                            def flavor = variant.flavorName.toString();
+                            if (flavor == null || flavor.trim().length() <= 0) {
+                                flavor = "gm";
+                            }
+
+                            if (isgm) {
+                                if (flavor == null || flavor.trim().length() <= 0) {
+                                    flavor = "gm";
+                                }
+                            }
+
+                            def apkFileName = "${buildTime}_${apkname}_V${variant.versionName}_${flavor}.apk";
+                            def apkFile = new File(output.outputFile.getParent(), apkFileName);
+                            output.outputFile = apkFile;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         System.out.println("========================");
 
     }
