b624004fcfb7be14d87d66a301d2893a4625b868
Tongson <289286298@qq.com>
2017-06-12 08:20:47 +0800

    mvp

---
+++ ts_play_application/src/main/java/pr/tongson/mvp/base/BaseFragment.java
@@ -0,0 +1,11 @@
+package pr.tongson.mvp.base;
+
+/**
+ * <b>Project:</b> ${file_name}<br>
+ * <b>Create Date:</b> 2017/6/12<br>
+ * <b>Author:</b> Tongson<br>
+ * <b>Description:</b> <br>
+ */
+
+public class BaseFragment {
+}
