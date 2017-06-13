533b5e562a5256ec205b5823331ba0143c8fb8e8
Tongson <289286298@qq.com>
2017-06-13 08:46:33 +0800

    update

--- tongsonimitate/src/main/java/com/example/tongsonimitate/demo/i/Operator.java
+++ tongsonimitate/src/main/java/com/example/tongsonimitate/demo/i/Operator.java
@@ -6,7 +6,7 @@ import com.example.tongsonimitate.demo.Subscriber;
  * <b>Project:</b> ${file_name}<br>
  * <b>Create Date:</b> 2017/5/27<br>
  * <b>Author:</b> Tongson<br>
- * <b>Description:</b> 观察者 <br>
+ * <b>Description:</b> 观察者-->项目组 <br>
  */
 
 public interface Operator<T, R> extends Func<Subscriber<? super T>, Subscriber<? super R>> {
