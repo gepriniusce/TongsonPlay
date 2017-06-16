b9a5a65f21ccd13c5400127aa575d71f8f515b3d
kongming <luzhanneng@linghit.com>
2017-06-16 11:21:06 +0800

    Website

--- ts_play_application/src/main/java/pr/tongson/MainActivity.java
+++ ts_play_application/src/main/java/pr/tongson/MainActivity.java
@@ -4,11 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /*
+    * http://www.jianshu.com/p/6e5eda3a51af
+    * http://www.runoob.com/design-pattern/design-pattern-tutorial.html
+    * http://download.csdn.net/detail/banketree/7338939
+    * 
+    * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
 }
