630dda633a493d3d88ef378f2fd0b03481011e31
Tongson <289286298@qq.com>
2017-08-10 00:31:59 +0800

    Website

--- ts_play_application/src/main/java/pr/tongson/MainActivity.java
+++ ts_play_application/src/main/java/pr/tongson/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import pr.tongson.View.Main3Activity;
-
+/**
+ * https://juejin.im/entry/58291283a22b9d00696ed518
+ * http://www.jianshu.com/p/c83e08dfb6fb
+ * */
 public class MainActivity extends AppCompatActivity {
 
     @Override
