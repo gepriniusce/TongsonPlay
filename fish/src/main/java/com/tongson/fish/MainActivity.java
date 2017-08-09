20dc59b60cb4bd05c8c3890427f6dbc23746fa72
Tongson <289286298@qq.com>
2017-08-09 21:50:44 +0800

    fish

---
+++ fish/src/main/java/com/tongson/fish/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tongson.fish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * http://www.jianshu.com/p/3dd3d1524851
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
