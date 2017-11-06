7ef772c6cf2e6337d3aca7d753a6d0d894f7b8ef
Tongson <289286298@qq.com>
2017-11-06 23:21:35 +0800

    Factory

---
+++ ts_play_application/src/main/java/pr/tongson/pattern/Singleton/EagerSingleton.java
@@ -0,0 +1,10 @@
+package pr.tongson.pattern.Singleton;
+
+/**
+ * <b>Create Date:</b> 2017/11/6<br>
+ * <b>Author:</b> Tongson <br>
+ * <b>Email:</b> 289286298@qq.com<br>
+ * <b>Description:</b> 饿汉式单例类 <br>
+ */
+public class EagerSingleton {
+}
