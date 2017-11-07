af049dda611d9d948a8a292153838e466bc2f19b
Tongson <289286298@qq.com>
2017-11-07 09:07:37 +0800

    Singleton

--- ts_play_application/src/main/java/pr/tongson/pattern/Singleton/EagerSingleton.java
+++ ts_play_application/src/main/java/pr/tongson/pattern/Singleton/EagerSingleton.java
@@ -5,6 +5,29 @@ package pr.tongson.pattern.Singleton;
  * <b>Author:</b> Tongson <br>
  * <b>Email:</b> 289286298@qq.com<br>
  * <b>Description:</b> 饿汉式单例类 <br>
+ * 饿汉式单例类在自己被加载时将自己实例化。
+ * 即便加载器是静态的，在饿汉式单例类加载时仍会将自己实例化。
+ * 单从资源利用率角度来讲，这个比懒汉式单例类稍差些。
+ * 从速度和反应时间角度来讲，则比懒汉式单例类稍好些。
+ * 
+ * 不可继承
  */
 public class EagerSingleton {
+
+    private static final EagerSingleton mInstance = new EagerSingleton();
+
+    /**
+     * 私有的默认构造子
+     */
+    private EagerSingleton() {
+    }
+
+    /**
+     * 静态工厂方法
+     *
+     * @return
+     */
+    public static EagerSingleton getInstance() {
+        return mInstance;
+    }
 }
---
+++ ts_play_application/src/main/java/pr/tongson/pattern/Singleton/RegSingleton.java
@@ -0,0 +1,61 @@
+package pr.tongson.pattern.Singleton;
+
+import java.util.HashMap;
+
+/**
+ * <b>Create Date:</b> 2017/11/7<br>
+ * <b>Author:</b> Tongson <br>
+ * <b>Email:</b> 289286298@qq.com<br>
+ * <b>Description:</b>  <br>
+ */
+public class RegSingleton {
+    static private HashMap mRegistry = new HashMap();
+
+    static {
+        RegSingleton x = new RegSingleton();
+        mRegistry.put(x.getClass().getName(), x);
+    }
+
+    /**
+     * 保护的默认构造子
+     */
+    protected RegSingleton() {
+
+    }
+
+    /**
+     * 静态工厂方法，返还此类的唯一实例
+     *
+     * @param name
+     * @return
+     */
+    static public RegSingleton getInstance(String name) {
+        if (name == null) {
+            name = "pr.tongson.pattern.Singleton.RegSingleton";
+        }
+        if (mRegistry.get(name) == null) {
+            try {
+                mRegistry.put(name, Class.forName(name).newInstance());
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return (RegSingleton) mRegistry.get(name);
+    }
+
+
+    /**
+     * 一些示意性的商业方法
+     *
+     * @return
+     */
+    public String about() {
+        return "Hello, I am RegSigleton.";
+    }
+}
