476ab126772fa09e49e96af37bfcec9bf3d1abfa
Tongson <289286298@qq.com>
2018-01-03 23:33:08 +0800

    构造模式

--- ts_play_application/src/main/java/pr/tongson/pattern2/Bridge/RefinedAbstraction.java
+++ ts_play_application/src/main/java/pr/tongson/pattern2/Bridge/RefinedAbstraction.java
@@ -1,9 +1,6 @@
 package pr.tongson.pattern2.Bridge;
 
 /**
- * <b>Create Date:</b> 2017/12/26<br>
- * <b>Author:</b> Tongson <br>
- * <b>Email:</b> 289286298@qq.com<br>
  * <b>Description:</b> 修正抽象化 <br>
  */
 public class RefinedAbstraction extends Abstraction {
