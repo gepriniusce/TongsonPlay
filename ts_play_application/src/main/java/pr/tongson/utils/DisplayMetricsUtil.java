3bdf7e9511797d127964b36fe3bb341828e2d800
Tongson <luzhanneng@linghit.com>
2018-05-16 09:07:49 +0800

    DisplayMetrics

---
+++ ts_play_application/src/main/java/pr/tongson/utils/DisplayMetricsUtil.java
@@ -0,0 +1,42 @@
+package pr.tongson.utils;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * <b>Create Date:</b> 2018/4/26<br>
+ * <b>Email:</b> 289286298@qq.com<br>
+ * <b>Description:</b>  <br>
+ *
+ * @author mmc_Kongming_Tongson
+ */
+public class DisplayMetricsUtil {
+    public static int dp2px(Context context, int dpValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, context.getResources().getDisplayMetrics());
+    }
+
+    public static int sp2px(Context context, int spValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spValue, context.getResources().getDisplayMetrics());
+    }
+
+
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public static int dp2px(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5F);
+    }
+
+    public static int px2dp(Context context, float px) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (px / scale + 0.5F);
+    }
+}
